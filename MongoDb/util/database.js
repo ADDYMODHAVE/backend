ef74f605a10c925f122e5188ec03d6f6446dd0aa
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2023-04-14 22:27:12 +0530

    learned some about some operation still
    to learn more

--- MongoDb/util/database.js
+++ MongoDb/util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://Aditya_mongo:Aditya@123@aditya.hwwenod.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
   )
     .then(client => {
       console.log('Connected!');
